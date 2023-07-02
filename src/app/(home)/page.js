dd380f9ef45e7b93e4eec15cf827365ff9729fea
Sebastian Gonzalez <onemogen8@gmail.com>
2023-07-02 01:12:14 -0300

    css fix animation overflow

--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -3,7 +3,7 @@ import EmployeesTable from '@/components/employees-table/EmployeesTable';
 
 export default function HomePage() {
     return (
-        <div className='h-full w-full py-5'>
+        <div className='h-full w-full py-5 overflow-hidden'>
             <EmployeesTable></EmployeesTable>
         </div>
     )
--- src/components/employees-table/EmployeesTable.js
+++ src/components/employees-table/EmployeesTable.js
@@ -34,7 +34,7 @@ const EmployeesTable = () => {
     }
 
     return employees && (
-        <HorizontalRouteAnimator>
+        <HorizontalRouteAnimator className={''}>
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg mx-8">
                 <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead className="bg-gradient-to-r from-blue-500 to-pink-700 bg-opacity-10 text-xs text-white uppercase">
