f59ac8ce0f285c906afb40951e45e286531f6460
Sebastian Gonzalez <onemogen8@gmail.com>
2023-07-04 17:33:41 -0300

    edit employee seciton refactor

--- src/app/(home)/employee/[id]/edit/page.js
+++ src/app/(home)/employee/[id]/edit/page.js
@@ -1,4 +1,5 @@
 'use client'
+
 import { LoadingBackdrop, CardLoadingSpinner, ConfirmDialog, EmployeeForm, CustomToast } from "@/components";
 import { DIALOG_MODE } from "@/components/confirm-dialog/ConfirmDialog";
 import { TOAST_MODE } from "@/components/custom-toast/CustomToast";
@@ -12,99 +13,151 @@ import { useParams, useRouter } from "next/navigation";
 import { useContext, useEffect, useState } from "react";
 
 const EmployeeEditPage = () => {
-    let employee;
     const router = useRouter();
     const params = useParams();
     const { contextState } = useContext(EmployeeDataContext);
     const { updateContext: setToastData } = useContext(ToastNotificationContext);
     const [fetchEmployee, { loading, error, data }] = useEmployeeData(params.id);
-    const [formValues, setFormValues] = useState({})
+    const [state, setState] = useState({
+        employee: undefined,
+        formValues: {},
+        resetForm: false,
+        openConfirm: false,
+        openLoadingBackdrop: false,
+    });
     const [editEmployee] = useEditEmployee();
-    const [resetForm, setResetForm] = useState(false);
-    const [openConfirm, setOpenConfirm] = useState(false);
-    const [openLoadingBackdrop, setOpenLoadingBackdrop] = useState(false);
-
-    if (contextState.employeeData) employee = contextState.employeeData;
-    if (data) employee = data.employeeData.employee;
 
     useEffect(() => {
         if (!contextState.employeeData) {
-            //console.log('entra fetch');
             fetchEmployee();
+        } else {
+            setState((prevState) => ({
+                ...prevState,
+                employee: contextState.employeeData,
+            }));
         }
     }, []);
 
+    useEffect(() => {
+        if (data) {
+            setState((prevState) => ({
+                ...prevState,
+                employee: data.employeeData.employee,
+            }));
+        }
+    }, [data]);
+
     const handleSubmit = (values) => {
-        //console.log('handle submit', values);
-        setFormValues(values);
-        setOpenConfirm(true);
-    }
+        setState((prevState) => ({
+            ...prevState,
+            formValues: values,
+            openConfirm: true,
+        }));
+    };
 
     const handleConfirmDialog = () => {
-        setOpenConfirm(false);
-        setOpenLoadingBackdrop(true);
+        setState((prevState) => ({
+            ...prevState,
+            openConfirm: false,
+            openLoadingBackdrop: true,
+        }));
+
         setTimeout(() => {
-            const employeeInput = new EmployeeInput(formValues);
+            const employeeInput = new EmployeeInput(state.formValues);
             editEmployee({
                 variables: {
-                    data: employeeInput
+                    data: employeeInput,
                 },
                 onCompleted: (data) => {
-                    setOpenLoadingBackdrop(false);
+                    setState((prevState) => ({
+                        ...prevState,
+                        openLoadingBackdrop: false,
+                    }));
+
                     setToastData({
                         show: true,
                         message: "Employee register modified successfully",
-                        mode: TOAST_MODE.success
+                        mode: TOAST_MODE.success,
                     });
-                    setResetForm(true);
-                    router.push(ROUTES.viewEmployee + employee.id);
-                    //console.log(data);
+
+                    setState((prevState) => ({
+                        ...prevState,
+                        resetForm: true,
+                    }));
+                    console.log(data);
+
+                    router.push(ROUTES.viewEmployee + state.employee.id);
                 },
                 onError: (error) => {
-                    setOpenLoadingBackdrop(false);
+                    setState((prevState) => ({
+                        ...prevState,
+                        openLoadingBackdrop: false,
+                    }));
+
                     setToastData({
                         show: true,
                         message: "Employee register could not be modified",
-                        mode: TOAST_MODE.error
+                        mode: TOAST_MODE.error,
                     });
-                    console.error('Error creating user:', error);
+
+                    console.error("Error creating user:", error);
                 },
             });
         }, 3000);
-    }
+    };
+
+    const { employee, resetForm, openConfirm, openLoadingBackdrop } = state;
+
+    //console.log(employee);
+    //console.log(contextState.employeeData);
 
     return (
         <>
-            {
-                openLoadingBackdrop
-                && <LoadingBackdrop open={openLoadingBackdrop}></LoadingBackdrop>
-            }
-            {
-                openConfirm &&
+            {openLoadingBackdrop && <LoadingBackdrop open={openLoadingBackdrop} />}
+            {openConfirm && (
                 <ConfirmDialog
                     open={openConfirm}
-                    setOpen={setOpenConfirm}
+                    setOpen={(value) =>
+                        setState((prevState) => ({
+                            ...prevState,
+                            openConfirm: value,
+                        }))
+                    }
                     handleConfirm={handleConfirmDialog}
                     mode={DIALOG_MODE.edit}
-                >
-                </ConfirmDialog >
-            }
-            {(loading || employee === undefined) && <div className="flex h-full items-start justify-center w-full">
-                <CardLoadingSpinner />
-            </div>}
-            {error &&
-                <div className="flex justify-center items-center">
-                    <h2>There's been a problem loading the data.</h2>
+                />
+            )}
+            {(loading || employee === undefined) && (
+                <div className="flex h-full items-start justify-center w-full">
+                    <CardLoadingSpinner />
+                </div>
+            )}
+            {employee === null || error &&
+                <div className="flex justify-center">
+                    <div className="w-full max-w-2xl">
+                        <div className="my-3 p-8 card-shadow bg-white border border-gray-300 rounded-lg dark:bg-gray-800 dark:border-gray-700">
+                            <div className="flex justify-center items-center">
+                                {error && <h2>There's been a problem loading the data.</h2>}
+                                {employee === null &&
+                                    <h2>An employee with such ID does not exist.</h2>}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             }
-            {employee && employee != null && <div className="flex justify-center">
-                <div className="w-full max-w-2xl">
-                    <EmployeeForm employeeData={employee} resetForm={resetForm} handleSubmit={handleSubmit}></EmployeeForm>
+            {employee && employee !== null && (
+                <div className="flex justify-center">
+                    <div className="w-full max-w-2xl">
+                        <EmployeeForm
+                            employeeData={employee}
+                            resetForm={resetForm}
+                            handleSubmit={handleSubmit}
+                        />
+                    </div>
                 </div>
-            </div>}
+            )}
         </>
-
     );
-}
+};
 
 export default EmployeeEditPage;
