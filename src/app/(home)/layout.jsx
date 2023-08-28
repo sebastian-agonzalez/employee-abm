aec226e6da8f8686306d7b61c71a00c1a3d37a10
Sebastian Gonzalez <onemogen8@gmail.com>
2023-08-28 18:37:14 -0300

    fix animation layout conflict

--- src/app/(home)/layout.jsx
+++ src/app/(home)/layout.jsx
@@ -49,49 +49,44 @@ export default function HomeLayout({ children }) {
 
   return (
     <>
-      <div className="relative h-screen w-full overflow-hidden">
-        {/*  */}
-        <div
-          id="portrait-placeholder"
-          className={
-            "flex justify-center items-center w-full h-screen absolute top-0 left-0 right-0"
-          }
-        >
-          <div className="rotate-90 text-center animate-pulse">
-            <AppLogo />
-            <div className="my-4"></div>
-            <p className="text-xl">
-              Rotate screen to use app{" "}
-              <AiOutlineRotateLeft className="inline"></AiOutlineRotateLeft>
-            </p>
-          </div>
+      {/*  */}
+      <div
+        id="portrait-placeholder"
+        className={"flex justify-center items-center w-full h-screen"}
+      >
+        <div className="rotate-90 text-center animate-pulse">
+          <AppLogo />
+          <div className="my-4"></div>
+          <p className="text-xl">
+            Rotate screen to use app{" "}
+            <AiOutlineRotateLeft className="inline"></AiOutlineRotateLeft>
+          </p>
         </div>
-        {/*  */}
-        <div
-          id="app-content"
-          className="flex-col h-screen absolute top-0 left-0 right-0"
-        >
-          <Header></Header>
-          <main className="flex-1 my-1 bg-gradient-to-r from-pink-200 to-blue-200 flex flex-col">
-            <section className="bg-white">
-              <ActionStatsBar></ActionStatsBar>
-            </section>
-            <section className="grid-bg flex-1">
-              <div className="my-6"></div>
-              <div className="pb-10">{children}</div>
-            </section>
-            {toastData?.show && (
-              <CustomToast
-                setToastData={setToastData}
-                mode={toastData.mode}
-                message={toastData.message}
-              ></CustomToast>
-            )}
-          </main>
-        </div>
-        {/*  */}
-        {showLoadingScreen && (
-          <>
+      </div>
+      {/*  */}
+      <div id="app-content" className="flex-col h-screen">
+        <Header></Header>
+        <main className="flex-1 my-1 bg-gradient-to-r from-pink-200 to-blue-200 flex flex-col">
+          <section className="bg-white">
+            <ActionStatsBar></ActionStatsBar>
+          </section>
+          <section className="grid-bg flex-1">
+            <div className="my-6"></div>
+            <div className="pb-10">{children}</div>
+          </section>
+          {toastData?.show && (
+            <CustomToast
+              setToastData={setToastData}
+              mode={toastData.mode}
+              message={toastData.message}
+            ></CustomToast>
+          )}
+        </main>
+      </div>
+      {/*  */}
+      {showLoadingScreen && (
+        <>
+          <div className="fixed top-0 left-0 h-full w-full z-50">
             <div className="absolute top-0 left-0 right-0 h-full w-full flex">
               <motion.div
                 animate={{ opacity: 0 }}
@@ -129,9 +124,9 @@ export default function HomeLayout({ children }) {
                 className="h-full w-full bg-zinc-900"
               ></motion.div>
             </div>
-          </>
-        )}
-      </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
